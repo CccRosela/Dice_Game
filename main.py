25409b9f38e38d4cb09c1c0b12165275a2a3b033
Rosela Cocoli <147844613+CccRosela@users.noreply.github.com>
2026-08-20 13:22:24 +0200

    small changes to logic in main.py
    Implemented main game loop and epilogue handling.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # main logic (functions will be added at a later point
 # will make 3 classes -> character, and 2 job classes (warrior, sage)
 # goal is to do 20 explorations to reach the end of the game
-
 def main():
   player = create_player() # needs to be done
 
-# while player is alive and explorations < 20: 
-  # ..... 
-  # event happens 
-  # explorations += 1
+  while player.is_alive() and explorations < 20:  # player.is_alive(), a character class function
+   # event happens 
+    explorations += 1
+    pass
 
-# if player is alive at the end: 
-  # epilogue
-  # end mssg
+  if player.is_alive():P
+    epilogue_alive(player)
+  else:
+    epilogue_dead(player)
+
+if __name__ == "__main__":
+  main()
