df3a5e2a32bf12f54051f199489190687f7c28d5
Rosela Cocoli <147844613+CccRosela@users.noreply.github.com>
2026-08-20 13:07:10 +0200

    Initialize main.py with game structure and comments
    Added initial comments and structure for main logic. This is just the basic idea, as things/features get implemented, the main code will be updated

---
+++ main.py
@@ -0,0 +1,15 @@
+# main logic (functions will be added at a later point
+# will make 3 classes -> character, and 2 job classes (warrior, sage)
+# goal is to do 20 explorations to reach the end of the game
+
+def main():
+  player = create_player() # needs to be done
+
+# while player is alive and explorations < 20: 
+  # ..... 
+  # event happens 
+  # explorations += 1
+
+# if player is alive at the end: 
+  # epilogue
+  # end mssg
