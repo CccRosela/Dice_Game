2c6f80a4eb9a94cd3a697ead3953baa0ad4b0867
Rosela Cocoli <147844613+CccRosela@users.noreply.github.com>
2026-08-20 12:09:57 +0200

    Add enemy descriptions and stats

---
+++ enemies.py
@@ -0,0 +1,30 @@
+# Enemies
+STANDARD_ENEMIES = {
+    "Cracked Sentinel" : 
+    {
+      "description": "An ancient, moss-covered statue awakened by faint dungeon magic.",
+      "stats": {"HP": 40, "STR": 17, "END": 12, "INT": 12}  
+    },
+  
+    "Fractured Echo" : 
+    {
+        "description": "A shadowy version of yourself, formed from endless possibilities."
+    }
+}
+
+
+SAGE_ENEMIES = {
+    "Venomwing" :
+    {
+      "description": "Fast-moving, venomous bats that attack in packs.",
+      "stats": {"HP": 40, "STR": 6, "END": 4, "INT": 22}
+  },
+
+  
+WARRIOR_ENEMIES = {
+    "Rustborn Marauder" : 
+    {
+      "description": "A shadow soldier driven by blind rage. Wields a rusty blade.",
+      "stats": {"HP": 60, "STR": 22, "END": 12, "INT": 2}
+    }
+}
